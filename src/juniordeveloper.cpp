25fcb91691aabb5f612ff7b6de581c777526e72e
LarsMuenkel <muenkel.lars-it24@it.dhbw-ravensburg.de>
2025-06-05 17:24:40 +0200

    created Files
    created and linked necessary files

---
+++ src/juniordeveloper.cpp
@@ -0,0 +1,1 @@
+#include "juniordeveloper.hpp"
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <vector>
+#include <memory> 
+#include "developer.hpp"
+#include "juniordeveloper.hpp"
+#include "seniordeveloper.hpp"
